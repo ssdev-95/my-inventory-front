592fd53c26a11cd99bf8b7feeb0ab1912180859a
xSallus <souza95salomao@gmail.com>
2020-01-26 15:37:00 -0400

    some ui files added/modified

--- App/Android/app/src/main/java/com/xsallus/myinventory/MainActivity.java
+++ App/Android/app/src/main/java/com/xsallus/myinventory/MainActivity.java
@@ -2,7 +2,9 @@ package com.xsallus.myinventory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler hand = new Handler();
+        hand.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2750);
     }
 }
