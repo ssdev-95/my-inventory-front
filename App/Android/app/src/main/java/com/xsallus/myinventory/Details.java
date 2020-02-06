49e2ce2f66e729746a22d0e93b82f9cbd8bd9556
xSallus <souza95salomao@gmail.com>
2020-02-06 14:13:01 -0400

    some hard working

--- App/Android/app/src/main/java/com/xsallus/myinventory/Details.java
+++ App/Android/app/src/main/java/com/xsallus/myinventory/Details.java
@@ -2,21 +2,21 @@ package com.xsallus.myinventory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class Home extends AppCompatActivity {
-
+public class Details extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_details);
     }
 
-    public void detailsCall(View v) {
-        Intent intent = new Intent(Home.this, Details.class);
+    public void prevScreen(View v) {
+        Intent intent = new Intent(Details.this, Home.class);
         startActivity(intent);
+        finish();
     }
 }
