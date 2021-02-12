4899fc9b78564a2f5cc0c67d2fbe55d661bbd577
xSallus <souza95salomao@gmail.com>
2021-02-12 16:48:52 -0400

    [ADDED] Drop-down menu for mobile version

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log('Hello devs!')
+const logo = document.querySelector('.logo')
+const dropDownMenu = document.querySelector('.drop-down')
+
+function dropToDown() {
+    let dropPosition = window.getComputedStyle(dropDownMenu).getPropertyValue('top')
+    const position = ['translateY(180px)',
+                      'translateY(-180px)']
+    //console.log(dropPosition)
+    switch(dropPosition) {
+        case '-180px':
+            dropDownMenu.style.setProperty('transform', position[0])
+            break
+        case '0px':
+            dropDownMenu.style.setProperty('transform', position[1])
+            break
+        default:
+            alert(`Something gone wrong.
+                   Try again later.`)
+            break
+    }
+}
+
+logo.addEventListener('click', dropToDown, false)
+
