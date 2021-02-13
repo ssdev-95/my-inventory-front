96227a7f6ea84da394c209002cd5d5756fffe56d
Salomao Souza <58428963+xSallus@users.noreply.github.com>
2021-02-12 22:26:01 -0400

    [ADD] Drop-down functionality with js file

--- js/index.js
+++ js/index.js
@@ -1,24 +1,20 @@
-const logo = document.querySelector('.logo')
-const dropDownMenu = document.querySelector('.drop-down')
+const logo = document
+                    .querySelector('.logo')
+const dropDownMenu = document
+                            .querySelector('.drop-down')
+const burger = document
+                      .querySelector('.burger')
 
-function dropToDown() {
-    let dropPosition = window.getComputedStyle(dropDownMenu).getPropertyValue('top')
-    const position = ['translateY(180px)',
-                      'translateY(-180px)']
-    //console.log(dropPosition)
-    switch(dropPosition) {
-        case '-180px':
-            dropDownMenu.style.setProperty('transform', position[0])
-            break
-        case '0px':
-            dropDownMenu.style.setProperty('transform', position[1])
-            break
-        default:
-            alert(`Something gone wrong.
-                   Try again later.`)
-            break
-    }
+
+function dropUpDown() {
+    dropDownMenu.classList.toggle('selected')
 }
 
-logo.addEventListener('click', dropToDown, false)
+logo.addEventListener('click',
+                      dropUpDown,
+                      false)
+
+burger.addEventListener('click',
+                        dropUpDown,
+                        false)
 
