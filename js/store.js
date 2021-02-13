0e08ec1d92f35215a640f262c33531f5f2935c60
Salomao Souza <souza95salomao@gmail.com>
2021-02-13 14:37:19 -0400

    [ADD] Store.js file for further storage coniguration

---
+++ js/store.js
@@ -0,0 +1,26 @@
+const data_list = [
+    {
+        prodName: 'Pomarola',
+        prodExpiration: '25/01/2032',
+        category: 'food',
+        quantity: '15und'
+    },
+    {
+        prodName: 'Agua Sanitaria',
+        prodExpiration: '01./12/2025',
+        category: 'cleaning',
+        quantity: '2und'
+    },
+    {
+        prodName: 'Creme Capilar',
+        prodExpiration: 05/07/2024,
+        category: 'hygiene',
+        quantity: '1und'
+    }
+]
+
+const Table = {
+    addData() {},
+    removeData() {},
+    updateData() {}
+}
