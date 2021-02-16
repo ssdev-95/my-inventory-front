74ddf3e1b139c88abb1531ec8ff15fc31db9e9ea
Salomao Souza <souza95salomao@gmail.com>
2021-02-16 12:28:08 -0400

    [ADD] Remove product function

--- js/store.js
+++ js/store.js
@@ -48,42 +48,44 @@ function pushData() {
       <td>${valiDate}</td>
       <td><img onclick="removeData('${categ}')" class="delete-btn" src="./assets/drawable/minus.svg" alt="Image minus"/></td>
    `
-    table.append(tr)
+    table.appendChild(tr)
 
     document.querySelector('.modal-overlay').classList.toggle('inactive')
     document.querySelector('.modal').innerHTML = ""
     document.querySelector('.modal').classList.toggle('register')
   } catch (e) {
     console.log(e.message)
-    alert(`
+    /*alert(`
       404 - Something went wrong...
                Please try again...
-    `)
+      Please remember, categories are Food, Hygiene and Cleaning,
+      or it's case variants, like food, FOOD, FoOd...
+    `)*/
   }
 }
 
 function removeData(category) {
-   //console.log(category) ok
-   //primeiro pegar a categoria
-   //com a categoria em maos,
-   //pegar a tabela expecifica da categoria
-   const table = document.querySelector(`.${category}-table #body`)
-   let index
-   //console.log(table.innerHTML)
-   try {
-   table.addEventListener('click', el => {
-     index = el.target.parentNode.parentNode.rowIndex
-     alert(`${category}-table:[${index}]`)
+   //console.log(`${category}`)
+   const tbody = document.querySelector(`.${category}-table .body`)
+   //console.log(tbody)
+   let node
+
+      tbody.addEventListener('click', el => {
+      if(el.target != el.currentTaget) {
+        try {
+          node = el.target.parentNode.parentNode
+          //console.log(node)
+          tbody.removeChild(node)
+        } catch (e) {
+            console.log(e.message)
+            alert(`
+            409 - Calm down buddy!
+                  The Bigbang occurred in a fraction of a second,
+                  but the dev don't work in that pace.
+                  I'll fix things soon, trust me i'm an engineer! :/
+            `)
+        }
+      }
+      //alert(`${category}-table:[${index}]`)
    })
-   } catch (e) {
-       console.log(e.message)
-       alert(`
-       409 - Calm down buddy!
-             The Bigbang occurred in a fraction of a second,
-             but the dev don't work in that pace.
-             I'll fix things soon, trust me i'm an engineer! :/
-       `)
-   }
-   //pegar da tabela o elemento filho que chamou a funcao
-   //remover o elemento selecionado
 }
