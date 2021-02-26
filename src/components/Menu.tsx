bee379c873bdc634a083fc039718a4e3c82242c3
Salomao Souza <souza95salomao@gmail.com>
2021-02-26 13:35:30 -0400

    alguma coisa foi alterada

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,29 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from '../styles/components/Menu.module.css';
 
 export function Menu() {
+    const [isDropdown, setIsDropdown] = useState(false)
     const [marginTop, setMarginTop] = useState(-225)
 
-    function drop() {
-        //const navBar = document.querySelector('.navBar')
-        if(marginTop==0) {
-            setMarginTop(-225)
+    const dropDown = () => {
+        if(marginTop == 0) {
+            setIsDropdown(false)
         } else {
-            setMarginTop(0)
+            setIsDropdown(true)
         }
-        console.log(marginTop)
+        console.log(isDropdown)
     }
+
+    useEffect(() => {
+        isDropdown ? (setMarginTop(-225)) : (setMarginTop(0))
+        console.log(marginTop)
+    }, [isDropdown])
 
     return (
         <div>
-            <nav style={{top: `${marginTop}px`}} className={styles.navBar}>
+            <nav className={styles.navBar}>
                 <ul>
                     <li>login</li>
                     <li>contact</li>
                     <li>about</li>
                 </ul>
             </nav>
-            <div className={styles.burger} onClick={drop}>
+            <div className={styles.burger} onClick={dropDown}>
                 <div className={styles.rectTop}></div>
                 <div className={styles.rectMid}></div>
                 <div className={styles.rectBot}></div>
