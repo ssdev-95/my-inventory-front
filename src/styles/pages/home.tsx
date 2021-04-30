e6ca0326ab6205da20bbaaa1c1fa5af3f34dc7ca
Salomao Souza <souza95salomao@gmail.com>
2021-04-30 09:40:12 -0400

    Working on send to database

--- src/styles/pages/home.tsx
+++ src/styles/pages/home.tsx
@@ -7,10 +7,9 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     color: ${colors.halfWhite};
-
     background: linear-gradient(239.54deg, ${colors.softBlue} 0%, ${colors.softGray} 100%);
-`
-
+    `
+    // 
 export const Main = styled.main`
     width: 100%;
     height:100%;
