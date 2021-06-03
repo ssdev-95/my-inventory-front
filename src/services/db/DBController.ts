74d2963a44067b78a1ea8a6dd1d8b55ac81e3e36
xSallus <souza95salomao@gmail.com>
2021-06-03 15:55:36 -0400

    [UPDATE] Database Controller

--- src/services/db/DBController.ts
+++ src/services/db/DBController.ts
@@ -1,13 +1,33 @@
 import { database } from "@/services/db/DBConfig";
+import { Product } from "@/types";
+
+const collname = process.env.NEXT_DB_COLLECTION
 
 export const Controller = {
   async get() {
-    const data = await database.collection("products").get()
-    const results = data.docs
+    const data = await database
+      .collection(process.env.NEXT_DB_COLLECTION)
+      .get();
+    const results: Product[] = data.docs.map((prod) => {
+      return {
+        id: prod.id,
+        name: prod.data().name,
+        category: prod.data().category,
+        quantity: prod.data().quantity,
+        expiration: prod.data().expirateon,
+      };
+    });
 
-    return results
+    return results;
   },
-  async set() {},
-  async update() {},
-  async delete() {},
+  async set(product: Product) {
+    const producto = {
+      ...product,
+      expirateon: product.expiration,
+    };
+
+    return await database.collection(collname).add(producto);
+  },
+  async update(id: string) {},
+  async delete(id: string) {},
 };
