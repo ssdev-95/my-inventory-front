c9a0e093e56b265aef3f3a20744131710a610dcb
xSallus <souza95salomao@gmail.com>
2021-06-03 15:54:45 -0400

    [UPDATE] Add Product Modal Context

--- src/contexts/AddProductModalContext.tsx
+++ src/contexts/AddProductModalContext.tsx
@@ -1,36 +1,36 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState } from 'react'
+import { useRouter } from 'next/router'
 
-import { AddProductModalContextData, ProviderProps } from '@/types'
+import { Controller } from '@/services/db/DBController'
+
+import { AddProductModalContextData, ProviderProps, Product } from '@/types'
 
 export const AddProductModalContext = createContext({} as AddProductModalContextData)
 
 export function AddProductModalProvider({children}: ProviderProps) {
     const [isAddModalOpen, setIsAddModalOpen] = useState(false)
-    const [newProduct, setNewProduct] = useState(null)
+    const router = useRouter()
 
     const toggleModal = () => {
         setIsAddModalOpen(!isAddModalOpen)
     }
 
     const submit = (data:any) => {
-        setNewProduct(data)
-        toggleModal()
-    }
-
-    const createProduct = async () => {
-        const product = {
-            name: newProduct?.name,
-            quantity: newProduct?.quantity,
-            category: newProduct?.category,
-            expirateon: String(newProduct?.expiration)
+        const { name,  category, quantity, expiration } = data
+        const product: Product = {
+            name: name,
+            quantity: Number(quantity),
+            category: category,
+            expiration: expiration
         }
-        
-        console.log(JSON.stringify(product))
-    }
 
-    useEffect(()=>{
-        if(newProduct) createProduct()
-    }, [newProduct])
+        Controller.set(product)
+            .then(res=>alert('Sucess'))
+            .catch(err=>console.log(err))
+        
+        toggleModal()
+        router.push('/')
+    }
 
     return (
         <AddProductModalContext.Provider value={{
