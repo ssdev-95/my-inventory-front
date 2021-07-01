b09ef323a3555910ffa5f9759fb5b0dc5c8a5d54
xSallus <souza95salomao@gmail.com>
2021-07-01 12:55:30 -0400

    [UPDATE] Some core files

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './pages/App';
+import reportWebVitals from './pages/reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
