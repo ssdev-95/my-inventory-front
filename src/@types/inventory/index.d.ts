7fd4b51ecf4dc3003ec1311efec5fae81cde2ea3
xSallus <souza95salomao@gmail.com>
2021-07-01 12:58:04 -0400

    [UPDATE] Types declarations files

---
+++ src/@types/inventory/index.d.ts
@@ -0,0 +1,52 @@
+import { ReactNode } from 'react'
+
+declare namespace Inventory {
+    export interface Product {
+        id?: string;
+        name: string;
+        category: string;
+        quantity: number;
+        expiration: string;
+    }
+    
+    export interface HomeProps {
+        products: Product[];
+    }
+    
+    export interface ProviderProps {
+        children: ReactNode
+    }
+    
+    export interface AddProductModalContextData {
+        isAddModalOpen: boolean;
+        toggleModal: ()=>void;
+        submit: (data:any)=>void;
+    }
+    
+    export interface ContactModalContextData {
+        isContactModalOpen: boolean;
+        toggleContactModal: ()=>void;
+    }
+    
+    export interface EditModalContextData {
+        isEditModalOpen: boolean;
+        toggleEditModal: (product:any)=>void;
+        updateProduct: (updatedProduct:any)=>void;
+        toUpdate: any;
+    }
+    
+    export interface ThemeContextData {
+        theme: string;
+        toggleTheme: ()=>void;
+    }
+    
+    export interface DashboardProps {
+        products: Product[];
+    }
+    
+    export interface HeaderProps {
+        pathA: string;
+        pathB: string;
+    }
+    
+}
