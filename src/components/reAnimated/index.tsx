3aa17a432b6d567c40ec25a82e7ca68969cc2c92
xSallus <souza95salomao@gmail.com>
2021-07-06 16:32:33 -0400

    [UPDATE] ReAnimated animation

--- src/components/reAnimated/index.tsx
+++ src/components/reAnimated/index.tsx
@@ -3,9 +3,9 @@ import styles from './reanimated.module.scss'
 
 export function ReAnimated() {
     return (
-        <div>
-            <SettingsIcon className={styles.iconbigger} />
-            <SettingsIcon className={styles.iconsmaller} />
+        <div className={styles.animated}>
+            <SettingsIcon />
+            <SettingsIcon />
         </div>
     )
 }
