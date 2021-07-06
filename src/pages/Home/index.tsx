bd4b926f6232ec7c1ecfc7d0e10a730776bfa21b
xSallus <souza95salomao@gmail.com>
2021-07-06 16:31:15 -0400

    [UPDATE] Home component switching

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from 'react'
-
 import { useNavigation } from 'src/hooks/useNavigation'
 import { useAuth } from 'src/hooks/useAuth'
 
@@ -14,15 +12,16 @@ import { CProductList } from 'src/components/Dashboard/ProductList'
 import { CContact } from 'src/components/Dashboard/Contact'
 
 import styles from './home.module.scss'
+import { useEffect } from 'react'
 
 export function Home() {
     const { currentComponent, changeCurrentComponent } = useNavigation()
     const { user } = useAuth()
 
-    useEffect(()=>{
+    useEffect(()=> {
         if(!user) {
             changeCurrentComponent('Login')
-            return
+            return;
         }
     }, [])
 
@@ -32,12 +31,13 @@ export function Home() {
         >
             <Header />
             <Container>
-                { (currentComponent==='AddProduct') && (<CAddProduct />) }
-                { (currentComponent==='About') && (<CAbout />) }
-                { (currentComponent==='Login') && (<CLogin />) }
-                { (currentComponent==='SignUp') && (<CSignUp />)}
-                { (currentComponent==='ProductList') && (<CProductList />) }
-                { (currentComponent==='Contact') && (<CContact />) }
+                { (currentComponent==='AddProduct' && user) && (<CAddProduct />) }
+                { (currentComponent==='About' && user) && (<CAbout />) }
+                { (currentComponent==='Login' && !user) && (<CLogin />) }
+                { (currentComponent==='SignUp' && !user) && (<CSignUp />)}
+                { (currentComponent==='ProductList' && user) && (<CProductList />) }
+                { (currentComponent==='Contact' && user) && (<CContact />) }
+                { (!currentComponent && !user) && (<div>LOL</div>) }
             </Container>
         </div>
     )
