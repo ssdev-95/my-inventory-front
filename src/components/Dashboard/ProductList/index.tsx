88b6e14b33a9e65c3874523bf6e9359d92d58d22
xSallus <souza95salomao@gmail.com>
2021-07-06 16:29:33 -0400

    [UPDATE] Product list component styles

--- src/components/Dashboard/ProductList/index.tsx
+++ src/components/Dashboard/ProductList/index.tsx
@@ -1,5 +1,37 @@
+import { useAuth } from 'src/hooks/useAuth'
+import { useNavigation } from 'src/hooks/useNavigation'
+
+import PlayArrowIcon from '@material-ui/icons/PlayArrow'
+
+import { CButton } from 'src/components/Button'
+
+import styles from './products.module.scss'
+
 export const CProductList = () => {
+    const { user } = useAuth()
+    const { changeCurrentComponent } = useNavigation()
+
     return (
-        <div>Products here</div>
+        <div className={styles.productsContainer}>
+            <header>
+                <div>
+                    <CButton>
+                        <PlayArrowIcon />
+                    </CButton>
+                    <span>Food</span>
+                    <CButton>
+                        <PlayArrowIcon />
+                    </CButton>
+                </div>
+                <CButton
+                  className={styles.addButton}
+                  onClick={()=>changeCurrentComponent('AddProduct')}
+                >
+                    <span>Add item</span>
+                </CButton>
+                <p>{`Wellcome back, ${(user?.name.split(' '))?.[0]}.`}</p>
+            </header>
+            <section>Products here</section>
+        </div>
     )
 }
