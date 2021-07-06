a2c149ee3619ae3f2fefe4bafda57c7efc68a26b
xSallus <souza95salomao@gmail.com>
2021-07-06 16:33:17 -0400

    [UPDATE] Header styles and structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { CButton } from 'src/components/Button'
 import DashboardIcon from '@material-ui/icons/Dashboard'
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom'
 
 import { useNavigation } from 'src/hooks/useNavigation'
+import { useAuth } from 'src/hooks/useAuth'
 
 import styles from './header.module.scss'
 
 export function Header() {
     const { changeCurrentComponent } = useNavigation()
+    const { user, handleLogout } = useAuth()
 
     return(
         <header className={styles.header}>
@@ -15,6 +18,16 @@ export function Header() {
             <div className={styles.menu}>
                 <CButton onClick={()=>changeCurrentComponent('Contact')}>Contact</CButton>
                 <CButton onClick={()=>changeCurrentComponent('About')}>About</CButton>
+                {
+                   user && (
+                    <CButton
+                      className={styles.exit}
+                      onClick={handleLogout}
+                    >
+                        <MeetingRoomIcon />
+                    </CButton>
+                   )
+                }
             </div>
         </header>
     )
