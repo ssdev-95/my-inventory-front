854c1fdb28b88e6ae85f0444d79fb838f76a10c9
xSallus <souza95salomao@gmail.com>
2021-07-08 11:15:22 -0400

    [UPDATE] Product Context to only retrieve products owned by current user

--- src/contexts/ProductContext.tsx
+++ src/contexts/ProductContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { createContext, useEffect, useState } from "react"
 import { useAuth } from 'src/hooks/useAuth'
@@ -6,36 +7,22 @@ import { database } from 'src/services/firebase'
 
 import { Inventory } from "src/@types/inventory"
 
-type FirebaseProducts = Record<string, Inventory.Product>
+type FirebaseProducts = Record<string, {
+    id: string;
+    name: string;
+    category: string;
+    quantity: number;
+    expiration: string;
+    owner: string;
+}>
 
 export const ProductContext = createContext({} as Inventory.ProductContext)
 
 export function ProductProvider({ children }: Inventory.ProviderProps) {
+    const filters = ["Food", "Hygiene", "Cleaning"]
     const [ products, setProducts ] = useState<Inventory.Product[]|undefined>()
+    const [currentSelection, setCurrentSelection] = useState(0)
     const { user } = useAuth()
-
-    function handleRetrieveData() {
-        const productsRef = database.ref('products')
-
-        productsRef.on('value', (entry)=>{
-            const firebaseProducts:FirebaseProducts = entry.val() ?? {}
-
-            const parsedProducts = Object.entries(firebaseProducts)
-
-            if(parsedProducts && parsedProducts.length>0) {
-                // To resolve filter product list that comes from database tomorrow :D
-                const items = parsedProducts.filter((item) => item[1].owner===user?.id)
-                console.log(parsedProducts)
-                // if (items) {
-                //     setProducts(items)
-                // }
-            }
-
-            return ()=>{
-                productsRef.off('value')
-            }
-        })
-    }
 
     async function handleDeleteData(id: string) {
         if(id.trim()!=='') {
@@ -46,18 +33,56 @@ export function ProductProvider({ children }: Inventory.ProviderProps) {
         return
     }
 
-    useEffect(()=>{
-        handleRetrieveData()
-    }, [])
+    function handleIncreaseSelection() {
+        if(currentSelection===2) {
+            setCurrentSelection(0)
+            return
+        }
+
+        setCurrentSelection(currentSelection+1)
+        return
+    }
+
+    function handleDecreaseSelection() {
+        if(currentSelection===0) {
+            setCurrentSelection(2)
+            return
+        }
+
+        setCurrentSelection(currentSelection-1)
+        return
+    }
 
     useEffect(()=>{
-        console.log(products)
-    }, [products])
+        const productsRef = database.ref('products')
+
+        productsRef.on('value', (entry)=>{
+            const firebaseProducts:FirebaseProducts = entry.val() ?? {}
+
+            const parsedProducts = Object.entries(firebaseProducts)
+            const filtered = parsedProducts.filter(item=>(item[1]).owner===user?.id)
+            const myProducts = filtered.map(([key, item])=>({
+                id: key,
+                name: item.name,
+                quantity: item.quantity,
+                category: item.category,
+                expiration: item.expiration
+            }))
+            setProducts(myProducts)
+        })
+
+        return ()=>{
+            productsRef.off('value')
+        }
+    }, [user])
 
     return(
         <ProductContext.Provider value={{
+            filters,
             products,
-            handleRetrieveData,
+            currentSelection,
+            handleIncreaseSelection,
+            handleDecreaseSelection,
             handleDeleteData
         }}>
             {children}
