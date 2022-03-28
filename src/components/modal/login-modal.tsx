4c2889e2f86e3b71ba2d188c8fb258fe494cabb4
Salomao S <souza95salomao@gmail.com>
2022-03-28 13:16:49 -0400

    [UPDATE] Some components improvements

--- src/components/modal/login-modal.tsx
+++ src/components/modal/login-modal.tsx
@@ -1,33 +1,33 @@
-import { Modal, Overlay } from "./base"
+import { ModalBase, Overlay } from "./base"
 import { SigninButton } from "../signin-button"
 
 interface IModalProps {
-	toggle: ()=>void;
+	toggle: () => void;
 	isOpen: boolean;
 }
 
-function LoginModal({ toggle, isOpen }: IModalProos) {
+function LoginModal({ toggle, isOpen }: IModalProps) {
 
 	return (
 		<Overlay isHidden={isOpen} >
-		  <Modal>
-     		<SigninButton
-    		  provider="Google"
-    			color="white"
-    			background="#f23"
-        />
-	    	<SigninButton
-      		provider="GitHub"
-	    		color="white"
-    			background="#1c1c1c"
-        />
-    		<SigninButton
-    		  provider="Discord"
-    			color="white"
-    			background="#33f"
-        />
+			<ModalBase>
+				<SigninButton
+					provider="Google"
+					color="white"
+					background="#f23"
+				/>
+				<SigninButton
+					provider="GitHub"
+					color="white"
+					background="#1c1c1c"
+				/>
+				<SigninButton
+					provider="Discord"
+					color="white"
+					background="#33f"
+				/>
 				<button className="close-button" onClick={toggle}>x</button>
-		  </Modal>
+			</ModalBase>
 		</Overlay>
 	)
 }
