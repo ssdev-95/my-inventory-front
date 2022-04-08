6914fa34c1c66eaa1f0e1d564e6d51268b75e062
Salomao S <souza95salomao@gmail.com>
2022-04-08 09:05:54 -0600

    [UPDATE] Modal functionalities

--- src/components/products/index.tsx
+++ src/components/products/index.tsx
@@ -2,7 +2,11 @@ import { useMemo } from "react"
 import { useProduct } from "../../contexts"
 import { Base, Table, THead, TBody, Row } from "./base"
 
-function Products() {
+interface ProdProps {
+  handler: () => void;
+}
+
+function Products({ handler }: ProdProps) {
   const { products, current } = useProduct()
 
 	const filtered = useMemo(()=>{
@@ -11,6 +15,15 @@ function Products() {
 
 		return filterings
 	},[current, products])
+
+	function handleClick(id: string) {
+	  const product = products.find(prod => prod.id === id)
+		localStorage.setItem(
+		  "@my_inventory:updating_item",
+			JSON.stringify(product)
+		)
+		handler()
+	}
 
   return (
 		<Base>
@@ -25,7 +38,10 @@ function Products() {
   			</THead>
 
   			<TBody>{filtered.map(item=>(
-  			  <Row id={item.id}>
+  			  <Row
+					  key={item.id}
+						onClick={() => handleClick(item.id)}
+					>
   				  <td>{item.name}</td>
 	  				<td>{item.quantity}</td>
 	  				<td>{item.expiration}</td>
